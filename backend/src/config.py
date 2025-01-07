a0475b1f06cf9572cf3a29f8711f1e2019a2d4a7
stefanspitse4pm <stefan@4propert.ie>
2025-01-07 18:26:56 +0100

    changed location of backend

--- backend/src/config.py
+++
@@ -1,4 +0,0 @@
-import os
-redis_host = os.getenv("REDIS_HOST", "localhost")
-redis_port = os.getenv("REDIS_PORT", 6379)
-
--- backend/src/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from tokens.router import router as tokens_router
-
-app = FastAPI()
-app.include_router(tokens_router)
-
