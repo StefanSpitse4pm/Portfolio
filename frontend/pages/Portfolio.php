3f5558647e07bfdd49a5466c4d44d7c83f3277ad
stefanspitse4pm <stefan@4propert.ie>
2025-01-14 13:11:44 +0100

    added files

--- frontend/pages/Portfolio.php
+++ frontend/pages/Portfolio.php
@@ -1,3 +1,12 @@
+<?php 
+
+if (empty($_SESSION['token'])) {
+    header('Location: index.html');
+} 
+
+?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,55 +27,64 @@
    <main id="content">
         <nav>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Portfolio</a></li>
-                <li><a href="">About me</a></li>
-                <li><a href="">Contact me</a></li>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="Portfolio.html">Portfolio</a></li>
+                <li><a href="aboutme.html">About me</a></li>
+                <li><a href="https://www.linkedin.com/in/stefan-spitse/">Contact me</a></li>
             </ul>
         </nav>
         
         <div class="main"> 
+            <a href="../files/reflectie verslag.pdf" target="_blank">
+                <div class="file">
+                    <p>reflectie Verslag</p>
+                </div>
+            </a>
+            <a href="../files/Feedback van periode 1.pdf" target="_blank">
+                <div class="file">
+                    <p>feed back periode 1</p>
+                </div>
+            </a>
+            <a href="../files/Belbin feedback periode 1.pdf" target="_blank">
+                <div class="file">
+                    <p>belbin feedback periode 1</p>
+                </div>
+            </a>
+            <a href="" target="_blank">
+                <div class="file">
+                    <p>Feedback periode 2</p>
+                </div>
+            </a>
+            <a href="../files/Plan van aanpak.pdf" target="_blank">
+                <div class="file">
+                    <p>Plan van aanpak</p>
+                </div>
+            </a>
+            <a href="../files/presentatie voorbereiding.pdf" target="_blank">
+                <div class="file">
+                    <p>Groeps presentatie voorbereiding</p>
+                </div>
+            </a>
+            <a href="../files/GroepspresentatieINF1B.pdf" target="_blank">
+                <div class="file">
+                    <p>Positief feedback groeps presentatie</p>
+                </div>
+            </a>
+            <a href="../files/agenda.pdf" target="_blank">
+                <div class="file">
+                    <p>Agenda vegadering</p>
+                </div>
+            </a>
             <a href="../files/Notulen_plenair_17_december_2024.pdf" target="_blank">
                 <div class="file">
                     <p>Notulen</p>
                 </div>
-            </a><a href="">
+            </a><a href="../files/Portfolio 2022_2023(2).pdf" target="_blank">
                 <div class="file">
-                    <p></p>
+                    <p>Edumundo opdrachten</p>
                 </div>
-            </a><a href="">
-                <div class="file">
-                    <p></p>
-                </div>
-            </a><a href="">
-                <div class="file">
-                    <p></p>
-                </div>
-            </a><a href="">
-                <div class="file">
-                    <p></p>
-                </div>
-            </a><a href="">
-                <div class="file">
-                    <p></p>
-                </div>
-            </a><a href="">
-                <div class="file">
-                    <p></p>
-                </div>
-            </a><a href="">
-                <div class="file">
-                    <p></p>
-                </div>
-            </a><a href="">
-                <div class="file">
-                    <p></p>
-                </div>
-            </a><a href="">
-                <div class="file">
-                    <p></p>
-                </div>
-            </a><a href="">
+            </a>
+            <a href="../files/">
                 <div class="file">
                     <p></p>
                 </div>
